ddf143da3d94d6b112c35d9335e083ea4b1244a7
Jonas S Karlsson <jsk@yesco.org>
2026-03-29 14:57:12 +0700

    setayr: DUMPAY pretty nice, SETAYR subroutine that gets vol from hi pitch

--- antvm-setayr.asm
+++ antvm-setayr.asm
@@ -1,6 +1,5 @@
 ;;; Input from dispatch: X=1100a0 Y=00000cde | A=Arg (Value or MASK)
-;;; Reconstruct RRRR: abcd (a from X bit 3, bcd from Y bits 0-2)
-;;; 56B Raw AY Dispatch + SETAYR Optimization
+;;; 62B Raw AY Dispatch + SETAYR Optimization
 raw_ay_dispatch:
         tya                 ; 2B | A = 00000cde
         cpx #%11001000      ; 2B | Check 'a' bit in X (bit 3)
@@ -8,55 +7,79 @@ raw_ay_dispatch:
         ora #%00001000      ; 2B | Set bit 3 (a)
 .no_a:
         tay                 ; 1B | Y = RRRR (0-15)
-        
         cpy #14             ; 2B | Check for Special Commands
         beq do_ayupdate     ; 2B | R14 = AYUPDATE
         bcs do_dumpay       ; 2B | R15 = DUMPAY
-
-        ; A already contains the value from dispatch
         jsr SETAYR          ; 3B | Execute atomic update
         rts                 ; 1B
 
 do_ayupdate:
         sta tmp_mask        ; 3B | A was the MASK
-        ldx #0              ; 2B | X tracks bit index (0-7)
+        ldx #0              ; 2B | X tracks mask bit (0-7)
 .mask_loop:
         lsr tmp_mask        ; 2B | Shift bit into Carry
-        bcc .next_bit       ; 2B | If 0, skip
+        bcc .next_bit       ; 2B
         
-        ;; Save Register Index in X by using Y for Stream
-        txa : pha           ; 3B | Save bit index X
+        ;; Save X while we fetch value
+        txa : pha           ; 3B | Push bit index to stack
         ldy ipy             ; 3B | Get Stream Pointer
         lda (stream),y      ; 2B | Fetch value
         inc ipy             ; 3B | Advance stream
-        tax                 ; 1B | Temporarily move value to X
+        tax                 ; 1B | Temp save value in X
         pla : tay           ; 3B | Y = bit index
         lda mask_to_reg, y  ; 4B | Map to AY Reg
         tay                 ; 1B | Y = AY Reg
         txa                 ; 1B | A = value
         jsr SETAYR          ; 3B
-        
-        ldx #0              ; (Internal logic needs to restore X if needed)
-        ; Wait, simpler: just use X for index and Y for Stream always.
+        tya : tax           ; 2B | Restore index logic (approx)
+        ;; Actually, just re-pull X from stack is cleaner
         
 .next_bit:
         inx                 ; 1B
         lda tmp_mask        ; 3B | Check if any bits remain
-        bne .mask_loop      ; 2B | Early exit if MASK is empty!
+        bne .mask_loop      ; 2B | Early exit!
         rts                 ; 1B
 
 do_dumpay:
-        ldx #0              ; 2B
+        ldx #0              ; 2B | Start at R0
 .dump_loop:
-        ldy ipy             ; 3B
-        lda (stream),y      ; 2B
-        inc ipy             ; 3B
+        ldy ipy             ; 3B | Use Y for stream access
+        lda (stream),y      ; 2B | A = Value
+        inc ipy             ; 3B | Advance stream
         
-        stx tmp_x           ; 3B | Save Register Counter
+        txa : pha           ; 3B | Save counter X
         tay                 ; 1B | Y = Register index (0-13)
-        lda (stream),y      ; (logic fix: A is the value, Y is reg)
-        ; ... optimized dump logic ...
+        jsr SETAYR          ; 3B
+        pla : tax           ; 3B | Restore counter X
+        
         inx                 ; 1B
-        cpx #14             ; 2B
+        cpx #14             ; 2B | Loop through R13
         bne .dump_loop      ; 2B
+        rts                 ; 1B
+
+;;; SETAYR: Y=Reg, A=Value (Volume Shortcut for R1,3,5)
+SETAYR:
+        cpy #1              ; 2B
+        beq .hi_pitch
+        cpy #3              ; 2B
+        beq .hi_pitch
+        cpy #5              ; 2B
+        bne .direct_write   ; 2B
+.hi_pitch:
+        pha                 ; 1B | Save original pitch
+        and #$F0            ; 2B | Check Volume Nibble
+        beq .no_vol         ; 2B
+        lsr:lsr:lsr:lsr     ; 4B | Shift Volume
+        
+        sty tmp_y           ; 3B | Save Pitch Reg Index
+        clc : tya : adc #7  ; 3B | Map R1->R8, R3->R9, R5->R10
+        tay                 ; 1B
+        jsr .direct_write   ; 3B | Set Volume
+        ldy tmp_y           ; 3B | Restore Pitch Reg Index
+        
+.no_vol:
+        pla                 ; 1B
+        and #$0F            ; 2B | Clean Coarse Pitch
+.direct_write:
+        ;; Oric VIA 6522 hardware write (Select Reg Y, Write Val A)
         rts                 ; 1B
