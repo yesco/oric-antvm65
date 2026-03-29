31fb4858ba35aee9738baa737a50006b51d0a381
Jonas S Karlsson <jsk@yesco.org>
2026-03-29 15:25:12 +0700

    setayr: optimized vol setting

--- antvm-setayr.asm
+++ antvm-setayr.asm
@@ -1,16 +1,16 @@
 ;;; Input from dispatch: X=1100a0 Y=00000cde | A=Arg (Value or MASK)
-;;; 46B Raw AY Dispatch + SETAYR Optimization
+;;; 64B Raw AY Dispatch + Zero-Carry Optimized SETAYR
 raw_ay_dispatch:
-        tya                 ; 2B | A = 00000cde
-        cpx #%11001000      ; 2B | Check 'a' bit in X
+        tya                 ; 2B | Reconstruct Reg Index
+        cpx #%11001000      ; 2B | Merge bit 3 from X
         bcc .no_a           
-        ora #%00001000      ; 2B | Merge bit 3
+        ora #%00001000      ; 2B 
 .no_a:
         tay                 ; 1B | Y = RRRR (0-15)
         cpy #14             ; 2B | R14=AYUPDATE, R15=DUMPAY
         beq do_ayupdate     
         bcs do_dumpay       
-        jsr SETAYR          ; 3B | Atomic update
+        jsr SETAYR          ; 3B 
         rts                 
 
 do_ayupdate:
@@ -19,7 +19,6 @@ do_ayupdate:
 .mask_loop:
         lsr tmp_mask        ; 2B | Bit into Carry
         bcc .next_bit       
-        
         txa : pha           ; 3B | Save bit index
         ldy ipy             ; 3B | Get stream
         lda (stream),y      ; 2B
@@ -28,7 +27,6 @@ do_ayupdate:
         lda mask_to_reg, y  ; 4B | Map to AY Reg
         tay : txa           ; 2B | Y = Reg, A = Value
         jsr SETAYR          
-        
 .next_bit:
         inx                 ; 1B
         lda tmp_mask        ; 3B
@@ -36,42 +34,43 @@ do_ayupdate:
         rts                 
 
 do_dumpay:
-        ldx #0              ; 2B | Start R0
+        ldx #0              ; 2B
 .dump_loop:
         ldy ipy             ; 3B
         lda (stream),y      ; 2B
         inc ipy             ; 3B
-        txy                 ; 1B | Y = Reg (No save needed)
+        txa : tay           ; 2B | Y = Reg index
         jsr SETAYR          
         inx                 ; 1B
         cpx #14             ; 2B
         bne .dump_loop      
         rts                 
 
-;;; SETAYR: Y=Reg, A=Val (Vol Shortcut for R1,3,5)
-;;; 26B Logic
+;;; SETAYR: Y=Reg, A=Val
+;;; 19B Zero-Carry Volume Shortcut
 SETAYR:
-        cpy #6              ; 2B | Only R0-R5 are pitch
-        bcs .direct_write   
-        sty tmp_y           ; 3B | Save to check parity
-        lsr tmp_y           ; 2B | Shift bit 0 into Carry
-        bcc .direct_write   ; 2B | If even (0,2,4), no volume shortcut
-        
-        pha                 ; 1B | Is Odd (1,3,5). Save Pitch.
-        and #$F0            ; 2B | Check Volume Nibble
-        beq .no_vol         
-        lsr:lsr:lsr:lsr     ; 4B | Move Vol to low nibble
-        pha                 ; 1B | Save Vol
-        tya:clc:adc #7:tay  ; 3B | R1->R8, R3->R9, R5->R10
-        pla                 ; 1B | A = Vol
-        jsr .direct_write   
-        ldy tmp_y           ; 3B | Restore original (tmp_y was lsr'd!)
-        ;; wait, tmp_y was modified by LSR. Use original Y.
-        tya:sec:sbc #7:tay  ; 3B | Safer restore
-.no_vol:
-        pla : and #$0F      ; 2B | Clean Pitch
+        pha                 ; 1B | Save for potential volume nibble
+        jsr .direct_write   ; 3B | Write Pitch/Reg first
+        tya                 ; 1B | Move Reg to A
+        lsr                 ; 1B | C is now 0 for even, 1 for odd
+        bcc .exit           ; 2B | If even, Carry=0, exit immediately
+        ;; C is 1 here. Math: (Y/2)*2 + 8. 
+        cmp #3              ; 2B | If A >= 3 (Reg >= 7), exit
+        bcs .exit           
+        asl                 ; 1B | A = 0, 2, 4 (Carry now 0)
+        adc #8              ; 2B | A = 8, 9, 10 (Carry was 0, so no CLC needed)
+        tay                 ; 1B
+        pla                 ; 1B | Restore original value
+        lsr:lsr:lsr:lsr     ; 4B | Move Volume to low nibble
+        beq .done           ; 2B | If volume was 0, skip
+        jmp .direct_write   ; 3B | Tail-call write
+.exit:
+        pla                 ; 1B | Clean stack
+.done:
+        rts                 ; 1B
+
 .direct_write:
-        ;; Oric VIA sequence (Select Reg Y, Write Val A)
+        ;; Oric VIA hardware write (Select Reg Y, Write Val A)
         rts                 
 
 mask_to_reg: .byte 0, 2, 4, 6, 7, 8, 9, 10
