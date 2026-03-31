471d3e290c597e4849c944f3684882da8254392e
Jonas S Karlsson <jsk@yesco.org>
2026-03-31 21:38:45 +0700

    ANT: zzz xxx and STDERR for fail

--- abc2ant.pl
+++ abc2ant.pl
@@ -14,6 +14,7 @@ while (<>) {
     s/%.*//; s/^\s+|\s+$//g; 
     next if /^$/;
 
+    # 1. Headers (Line-based)
     if (/^K:(\d+)$/) {
         my $val = $1;
         foreach my $ch (@active_ch) { $octave_map[$ch] = $val; }
@@ -21,18 +22,31 @@ while (<>) {
         next;
     }
 
+    # 2. Procedures
     if (/^\$([a-zA-Z0-9_]+):/) {
         print ".proc $1\n";
         next;
     }
 
+    # 3. Tokens
     foreach my $token (split(/\s+/, $_)) {
-        next if $token eq "";
+        next if $token eq "" || $token eq "|"; # Skip empty/bars
+
+        # CASE: ABC Rests (z/x with numbers OR repeated zzz/xxx)
+        if ($token =~ /^([zx]+)(\d*)$/) {
+            my $chars = $1;
+            my $multiplier = $2 || 1;
+            my $total_ticks = length($chars) * $multiplier;
+            
+            my $bin = sprintf("%%11000%03b", $total_ticks & 0x07);
+            printf "  .byte %-10s ;; %-10s (Rest: %d ticks)\n", $bin, $token, $total_ticks;
+        }
 
         # CASE: Channel Select (|ABC)
-        if ($token =~ /^\|([ABCN]+)$/) {
+        elsif ($token =~ /^\|([ABCN]+)$/) {
             my %map = (A=>0, B=>1, C=>2, N=>3);
-            @active_ch = map { $map{$_} } split //, $1;
+            my @list = split //, $1;
+            @active_ch = map { $map{$_} } @list;
             foreach my $ch (@active_ch) {
                 my $bin = sprintf("%%11011%03b", $ch);
                 printf "  .byte %-10s ;; %-10s (Select %s)\n", $bin, $token, (qw/A B C N/)[$ch];
@@ -46,7 +60,7 @@ while (<>) {
             $note += 2 if $acc eq '^'; 
             $note -= 2 if $acc eq '_'; 
 
-            my $oct = $octave_map[$active_ch[0]]; # Use lead active channel
+            my $oct = $octave_map[$active_ch[0]]; # Reference lead channel
             $oct++ if $n_char =~ /[a-z]/;
             $oct += length($oct_mod) if $oct_mod =~ /'/;
             $oct -= length($oct_mod) if $oct_mod =~ /,/;
@@ -56,21 +70,28 @@ while (<>) {
             printf "  .byte %-10s ;; %-10s (Note:%d Oct:%d)\n", $val, $token, $note, $oct;
 
             if ($dur ne "") {
-                my $w_bin = sprintf("%%11000%03b", $dur);
-                printf "  .byte %-10s ;; %-10s (WAIT for %s)\n", $w_bin, "", $token;
+                my $w_bin = sprintf("%%11000%03b", $dur & 0x07);
+                printf "  .byte %-10s ;; %-10s (Wait for %s)\n", $w_bin, "", $token;
             }
         }
 
-        # CASE: Explicit Commands
+        # CASE: Explicit Commands (WAIT/VALUE)
         elsif ($token =~ /^(WAIT|VALUE)(\d+)$/) {
             my $prefix = ($1 eq "WAIT") ? "11000" : "11001";
-            my $bin = sprintf("%%%s%03b", $prefix, $2);
+            my $bin = sprintf("%%%s%03b", $prefix, $2 & 0x07);
             printf "  .byte %-10s ;; %-10s\n", $bin, $token;
         }
 
         # CASE: Return
         elsif ($token eq "RET") {
             printf "  .byte %%11111111 ;; %-10s\n.endproc\n\n", $token;
+        }
+
+        # FALLBACK: Failure
+        else {
+            my $err = ";; FAIL: $token";
+            printf "  %-18s ;; Unknown Token\n", $err;
+            print STDERR "AntVM65 Error: Unknown token '$token' at line $.\n";
         }
     }
 }
