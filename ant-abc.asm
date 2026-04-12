0cbced94abcf1c7c4e58138b85298c23d643bf6d
Jonas S Karlsson <jsk@yesco.org>
2026-04-13 05:58:20 +0700

    abc

--- ant-abc.asm
+++ ant-abc.asm
@@ -1,81 +1,75 @@
-; === Global Music State (Initialize once at top of file) ===
+; === Global Music State (Initialize at top of file) ===
 M_OCT  = 4
 M_LEN  = 4
 M_ACC  = 0
 M_VOL  = 12
 
 .macro GEN_ABC_SPN Arg
-    _i = 0
+    _i := 0
     .repeat .strlen(Arg)
         .if _i < .strlen(Arg)
-            _c = .strat(Arg, _i)
+            _c := .strat(Arg, _i)
 
-            ; --- 1. ACCIDENTALS (Prefix: ^, _) ---
+            ; --- 1. ACCIDENTALS ---
             .if _c = '^'
-                M_ACC = 1
-                _i = _i + 1
-                _c = .strat(Arg, _i)
+                M_ACC := 1
+                _i := _i + 1
+                _c := .strat(Arg, _i)
             .elseif _c = '_'
-                M_ACC = -1
-                _i = _i + 1
-                _c = .strat(Arg, _i)
+                M_ACC := -1
+                _i := _i + 1
+                _c := .strat(Arg, _i)
             .endif
 
             ; --- 2. NOTES (A-H, a-h, z/R/P) ---
-            _nc = _c
+            _nc := _c
             .if (_nc >= 'A' && _nc <= 'H') || (_nc >= 'a' && _nc <= 'h') || _nc = 'z' || _nc = 'R' || _nc = 'P'
-                _toct = M_OCT
+                _toct := M_OCT
                 
-                ; ABC Lowercase = Octave Up
                 .if _nc >= 'a' && _nc <= 'h'
-                    _toct = _toct + 1
-                    _nc = _nc - 32 ; Convert to uppercase for mapping
+                    _toct := _toct + 1
+                    _nc := _nc - 32
                 .endif
 
-                ; TODO: 24-quarter-tone mapping (C=0, D=4, E=8, F=10, G=14, A=18, H=22)
-                _nv = 0
+                _nv := 0
                 .if _nc = 'C'
-                    _nv = 0
+                    _nv := 0
                 .elseif _nc = 'D'
-                    _nv = 2
+                    _nv := 2
                 .elseif _nc = 'E'
-                    _nv = 4
+                    _nv := 4
                 .elseif _nc = 'F'
-                    _nv = 5
+                    _nv := 5
                 .elseif _nc = 'G'
-                    _nv = 7
+                    _nv := 7
                 .elseif _nc = 'A'
-                    _nv = 9
+                    _nv := 9
                 .elseif _nc = 'H' || _nc = 'B'
-                    _nv = 11
+                    _nv := 11
                 .elseif _nc = 'z' || _nc = 'R' || _nc = 'P'
-                    _nv = $FF ; Rest code
+                    _nv := $FF
                 .endif
 
-                ; --- 3. LOOK-AHEAD FOR MODIFIERS ---
-                ; TODO: Check _i+1 for ' or , (ABC Octave)
-                ; TODO: Check _i+1 for / (ABC Length)
-                ; TODO: Check _i+1 for digits (SPN Octave/Length)
+                ; TODO: Look-ahead for ' , / and digits
 
-                ; OUTPUT NOTE
                 .byte (_toct * 12) + _nv + M_ACC
-                M_ACC = 0 ; Reset accidental
+                M_ACC := 0 
 
-            ; --- 4. COMMANDS (O, L, V, T, W, E, S) ---
+            ; --- 4. COMMANDS ---
             .elseif _c = 'O' || _c = 'o' || _c = 'K'
-                _i = _i + 1
-                M_OCT = (.strat(Arg, _i) - '0')
+                _i := _i + 1
+                M_OCT := (.strat(Arg, _i) - '0')
             .elseif _c = 'L'
-                _i = _i + 1
-                M_LEN = (.strat(Arg, _i) - '0') ; TODO: Multi-digit L16
+                _i := _i + 1
+                M_LEN := (.strat(Arg, _i) - '0')
             .elseif _c = 'V' || _c = 'v'
-                _i = _i + 1
-                M_VOL = (.strat(Arg, _i) - '0') ; TODO: Hex support V0F
+                _i := _i + 1
+                M_VOL := (.strat(Arg, _i) - '0')
             .elseif _c = 'S'
-                .byte $FF ; Stop
+                .byte $FF
             .endif
 
-            _i = _i + 1
+            _i := _i + 1
         .endif
     .endrepeat
 .endmacro
