6c4d8c883afa6f60b566e1a2d34c6f02012a1861
Jonas S Karlsson <jsk@yesco.org>
2026-04-13 05:55:50 +0700

    abc

--- ant-abc.asm
+++ ant-abc.asm
@@ -1,72 +1,81 @@
-; --- Global State Initialization ---
-_CUR_OCT  = 4
-_CUR_LEN  = 4
-_PEND_ACC = 0
+; === Global Music State (Initialize once at top of file) ===
+M_OCT  = 4
+M_LEN  = 4
+M_ACC  = 0
+M_VOL  = 12
 
 .macro GEN_ABC_SPN Arg
-    .local @Char, @I, @TmpOct, @FinalNote, @NoteChar
-    
-    @I .set 0
+    _i = 0
     .repeat .strlen(Arg)
-        .if @I < .strlen(Arg)
-            @Char = .strat(Arg, @I)
+        .if _i < .strlen(Arg)
+            _c = .strat(Arg, _i)
 
-            ; --- 1. HANDLE ACCIDENTAL PREFIX (^, _) ---
-            .if @Char = '^'
-                _PEND_ACC := 1
-                @I .set @I + 1
-                @Char = .strat(Arg, @I)
-            .elseif @Char = '_'
-                _PEND_ACC := -1
-                @I .set @I + 1
-                @Char = .strat(Arg, @I)
+            ; --- 1. ACCIDENTALS (Prefix: ^, _) ---
+            .if _c = '^'
+                M_ACC = 1
+                _i = _i + 1
+                _c = .strat(Arg, _i)
+            .elseif _c = '_'
+                M_ACC = -1
+                _i = _i + 1
+                _c = .strat(Arg, _i)
             .endif
 
-            ; --- 2. HANDLE NOTES (A-H, a-h, z) ---
-            @NoteChar = @Char
-            .if (@NoteChar >= 'A' && @NoteChar <= 'H') || (@NoteChar >= 'a' && @NoteChar <= 'h') || @NoteChar = 'z'
-                @TmpOct = _CUR_OCT
+            ; --- 2. NOTES (A-H, a-h, z/R/P) ---
+            _nc = _c
+            .if (_nc >= 'A' && _nc <= 'H') || (_nc >= 'a' && _nc <= 'h') || _nc = 'z' || _nc = 'R' || _nc = 'P'
+                _toct = M_OCT
                 
                 ; ABC Lowercase = Octave Up
-                .if @NoteChar >= 'a' && @NoteChar <= 'h'
-                    @TmpOct = @TmpOct + 1
-                    @NoteChar = @NoteChar - 32 ; Convert to uppercase for mapping
+                .if _nc >= 'a' && _nc <= 'h'
+                    _toct = _toct + 1
+                    _nc = _nc - 32 ; Convert to uppercase for mapping
                 .endif
 
-                ; TODO: Map @NoteChar to 0-23 (Quarter tones) 
-                ; C=0, D=2, E=4, F=5, G=7, A=9, H=11 (Example semitones)
-                @FinalNote = 0 
-                .if @NoteChar = 'C'
-                    @FinalNote = 0
-                .elseif @NoteChar = 'D'
-                    @FinalNote = 2
-                .elseif @NoteChar = 'E'
-                    @FinalNote = 4
-                .elseif @NoteChar = 'F'
-                    @NoteChar = 5
-                ; ... etc ...
+                ; TODO: 24-quarter-tone mapping (C=0, D=4, E=8, F=10, G=14, A=18, H=22)
+                _nv = 0
+                .if _nc = 'C'
+                    _nv = 0
+                .elseif _nc = 'D'
+                    _nv = 2
+                .elseif _nc = 'E'
+                    _nv = 4
+                .elseif _nc = 'F'
+                    _nv = 5
+                .elseif _nc = 'G'
+                    _nv = 7
+                .elseif _nc = 'A'
+                    _nv = 9
+                .elseif _nc = 'H' || _nc = 'B'
+                    _nv = 11
+                .elseif _nc = 'z' || _nc = 'R' || _nc = 'P'
+                    _nv = $FF ; Rest code
                 .endif
 
                 ; --- 3. LOOK-AHEAD FOR MODIFIERS ---
-                ; TODO: Check @I+1 for ABC markers (',') or SPN numbers (A4)
-                ; TODO: Check @I+1 for ABC length (A/16) or SPN length (A416)
+                ; TODO: Check _i+1 for ' or , (ABC Octave)
+                ; TODO: Check _i+1 for / (ABC Length)
+                ; TODO: Check _i+1 for digits (SPN Octave/Length)
 
-                ; OUTPUT (Example: Octave * 12 + Note + Accidental)
-                .byte (@TmpOct * 12) + @FinalNote + _PEND_ACC
-                
-                _PEND_ACC := 0 ; Reset accidental
+                ; OUTPUT NOTE
+                .byte (_toct * 12) + _nv + M_ACC
+                M_ACC = 0 ; Reset accidental
 
-            ; --- 4. HANDLE STICKY COMMANDS (O, L, V, T, W, E) ---
-            .elseif @Char = 'O' || @Char = 'o' || @Char = 'K'
-                @I .set @I + 1
-                _CUR_OCT := (.strat(Arg, @I) - '0')
-            .elseif @Char = 'L'
-                @I .set @I + 1
-                ; TODO: Multi-digit capture for Length
-                _CUR_LEN := (.strat(Arg, @I) - '0')
+            ; --- 4. COMMANDS (O, L, V, T, W, E, S) ---
+            .elseif _c = 'O' || _c = 'o' || _c = 'K'
+                _i = _i + 1
+                M_OCT = (.strat(Arg, _i) - '0')
+            .elseif _c = 'L'
+                _i = _i + 1
+                M_LEN = (.strat(Arg, _i) - '0') ; TODO: Multi-digit L16
+            .elseif _c = 'V' || _c = 'v'
+                _i = _i + 1
+                M_VOL = (.strat(Arg, _i) - '0') ; TODO: Hex support V0F
+            .elseif _c = 'S'
+                .byte $FF ; Stop
             .endif
 
-            @I .set @I + 1
+            _i = _i + 1
         .endif
     .endrepeat
 .endmacro
