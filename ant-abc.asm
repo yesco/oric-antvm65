b60482cf178972b780acc91e3c519bba36fcf425
Jonas S Karlsson <jsk@yesco.org>
2026-04-13 05:54:02 +0700

    abc

--- ant-abc.asm
+++ ant-abc.asm
@@ -1,66 +1,72 @@
-; --- Global Parser State ---
-.set _CUR_OCT    = 4        ; Default Octave
-.set _CUR_LEN    = 4        ; Default Length (Denominator)
-.set _PEND_ACC   = 0        ; Pending accidental (^ = +1, _ = -1)
+; --- Global State Initialization ---
+_CUR_OCT  = 4
+_CUR_LEN  = 4
+_PEND_ACC = 0
 
 .macro GEN_ABC_SPN Arg
-    .local @Char, @I, @TmpOct, @TmpLen, @FinalNote
+    .local @Char, @I, @TmpOct, @FinalNote, @NoteChar
     
-    @I = 0
+    @I .set 0
     .repeat .strlen(Arg)
         .if @I < .strlen(Arg)
             @Char = .strat(Arg, @I)
 
-            ; --- ACCIDENTALS (Prefix) ---
+            ; --- 1. HANDLE ACCIDENTAL PREFIX (^, _) ---
             .if @Char = '^'
-                .set _PEND_ACC, 1
-                @I = @I + 1
+                _PEND_ACC := 1
+                @I .set @I + 1
                 @Char = .strat(Arg, @I)
             .elseif @Char = '_'
-                .set _PEND_ACC, -1
-                @I = @I + 1
+                _PEND_ACC := -1
+                @I .set @I + 1
                 @Char = .strat(Arg, @I)
             .endif
 
-            ; --- NOTE HANDLING ---
-            .if (@Char >= 'A' & @Char <= 'G') | @Char = 'H' | (@Char >= 'a' & @Char <= 'h')
+            ; --- 2. HANDLE NOTES (A-H, a-h, z) ---
+            @NoteChar = @Char
+            .if (@NoteChar >= 'A' && @NoteChar <= 'H') || (@NoteChar >= 'a' && @NoteChar <= 'h') || @NoteChar = 'z'
                 @TmpOct = _CUR_OCT
                 
-                ; 1. Handle ABC Case-Sensitivity
-                .if @Char >= 'a' & @Char <= 'h'
+                ; ABC Lowercase = Octave Up
+                .if @NoteChar >= 'a' && @NoteChar <= 'h'
                     @TmpOct = @TmpOct + 1
+                    @NoteChar = @NoteChar - 32 ; Convert to uppercase for mapping
                 .endif
 
-                ; 2. Map Letter to Base Index (0-11 or 0-23)
-                ; TODO: Implement NoteMapping(@Char) -> 0, 2, 4, 5, 7, 9, 11...
-                @FinalNote = 0 ; Placeholder
+                ; TODO: Map @NoteChar to 0-23 (Quarter tones) 
+                ; C=0, D=2, E=4, F=5, G=7, A=9, H=11 (Example semitones)
+                @FinalNote = 0 
+                .if @NoteChar = 'C'
+                    @FinalNote = 0
+                .elseif @NoteChar = 'D'
+                    @FinalNote = 2
+                .elseif @NoteChar = 'E'
+                    @FinalNote = 4
+                .elseif @NoteChar = 'F'
+                    @NoteChar = 5
+                ; ... etc ...
+                .endif
 
-                ; 3. Look-ahead for ABC modifiers (',', ''')
-                ; TODO: Loop @I+1 to catch multiple ,,, or ''' and adjust @TmpOct
+                ; --- 3. LOOK-AHEAD FOR MODIFIERS ---
+                ; TODO: Check @I+1 for ABC markers (',') or SPN numbers (A4)
+                ; TODO: Check @I+1 for ABC length (A/16) or SPN length (A416)
 
-                ; 4. Look-ahead for SPN modifiers (A4, A416)
-                ; TODO: If @I+1 is a digit, @TmpOct = Digit. 
-                ; TODO: If @I+2 is also a digit, start capturing @TmpLen.
-
-                ; 5. Handle ABC Length (A/16)
-                ; TODO: If @Char = '/', capture following digits for @TmpLen.
-
-                ; 6. OUTPUT: Final Value = (@TmpOct * 12) + @FinalNote + _PEND_ACC
+                ; OUTPUT (Example: Octave * 12 + Note + Accidental)
                 .byte (@TmpOct * 12) + @FinalNote + _PEND_ACC
                 
-                .set _PEND_ACC, 0 ; Reset accidental after note is placed
+                _PEND_ACC := 0 ; Reset accidental
 
-            ; --- STICKY COMMANDS ---
-            .elseif @Char = 'O' | @Char = 'o' | @Char = 'K'
-                @I = @I + 1
-                .set _CUR_OCT, .strat(Arg, @I) - '0'
+            ; --- 4. HANDLE STICKY COMMANDS (O, L, V, T, W, E) ---
+            .elseif @Char = 'O' || @Char = 'o' || @Char = 'K'
+                @I .set @I + 1
+                _CUR_OCT := (.strat(Arg, @I) - '0')
             .elseif @Char = 'L'
-                @I = @I + 1
-                ; TODO: Capture multi-digit length
-                .set _CUR_LEN, 4 
+                @I .set @I + 1
+                ; TODO: Multi-digit capture for Length
+                _CUR_LEN := (.strat(Arg, @I) - '0')
             .endif
 
-            @I = @I + 1
+            @I .set @I + 1
         .endif
     .endrepeat
 .endmacro
