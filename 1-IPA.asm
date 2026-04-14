6f67edf847857897864eb7e827faa3a87d414b55
Jonas S Karlsson <jsk@yesco.org>
2026-04-14 21:28:39 +0700

    1-IPA.asm using C N and P

--- 1-IPA.asm
+++ 1-IPA.asm
@@ -38,92 +38,127 @@
 ;;;       the "mouth" might be in the middle of a silent part
 ;;;       of the wave when the new note
 
+
+;;; TODO: phonem subroutiunes, this one must be phonem1 !!!
+
+SHOFT_SAW= CALL1
+
+swe_short_saw: 
+        .byte ENV,LOOP_SAW, WAIT5, RET
+
+
+LONG_SAW= CALL2
+
+swe_long_saw: 
+        .byte ENV,LOOP_SAW, WAIT3, RET
+
+
 ; --- A Group ---
 speech_a_short:                 ; /a/ (Hatt)
         .byte R11,$05, R12,$00
         .byte R4,$39, R5,$00
-        .byte NOISE,$0d, ENV,LOOP_SAW, WAIT4, RET
+        .byte NOISE,$0d
+        .byte SHORT_SAW
 speech_a_long:                  ; /ɑː/ (Hata)
         .byte R11,$05, R12,$00
         .byte R4,$34, R5,$00
-        .byte NOISE,$0c, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0c
+        .byte LONG_SAW
 
-; --- O Group ---
+
+; --- O Group --- ROUNDED
 speech_o_short:                ; /ʊ/ (Bott)
         .byte R11,$08, R12,$00
         .byte R4,$4f, R5,$00
-        .byte NOISE,$0e, ENV,LOOP_TRI, WAIT4, RET
+        .byte NOISE,$0e
+        .byte ENV,LOOP_TRI, WAIT5, RET
 speech_o_long:                  ; /uː/ (Bo)
         .byte R11,$07, R12,$00
         .byte R4,$4a, R5,$00
-        .byte NOISE,$0d, ENV,LOOP_TRI, WAIT4, RET
+        .byte NOISE,$0d
+        .byte ENV,LOOP_TRI, WAIT3, RET
 
-; --- U Group ---
+; --- U Group --- ROUNDED
 speech_u_short:                 ; /ɵ/ (Hund)
         .byte R11,$09, R12,$00
         .byte R4,$3d, R5,$00
-        .byte NOISE,$0e, WAIT4, ENV,LOOP_TRI, RET
+        .byte NOISE,$0e
+        .byte ENV,LOOP_TRI, WAIT5, RET
 speech_u_long:                  ; /ʉː/ (Hus)
         .byte R11,$0d, R12,$00
         .byte R4,$29, R5,$00
-        .byte NOISE,$0e, WAIT4, ENV,LOOP_TRI, RET
+        .byte NOISE,$0e
+        .byte ENV,LOOP_TRI, WAIT3, RET
+
 
 ; --- UE / Y Group ---
 speech_ue_short:                ; /ʏ/ (German ü / Sytt)
         .byte R11,$0f, R12,$00
         .byte R4,$2b, R5,$00
-        .byte NOISE,$0c, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0c
+        .byte SHORT_SAW
 speech_ue_long:                 ; /yː/ (German ü / Syla)
         .byte R11,$11, R12,$00
         .byte R4,$29, R5,$00
-        .byte NOISE,$0b, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0b
+        .byte LONG_SAW
 
 ; --- E Group ---
 speech_e_short:                 ; /ɛ/ (Hetta)
         .byte R11,$08, R12,$00
         .byte R4,$25, R5,$00
-        .byte NOISE,$0d, WAIT4, ENV,LOOP_SAW, RET 
+        .byte NOISE,$0d
+        .byte SHORT_SAW
 speech_e_long:                  ; /eː/ (He)
         .byte R11,$0b, R12,$00
         .byte R4,$1e, R5,$00
-        .byte NOISE,$0b, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0b
+        .byte LONG_SAW
 
 ; --- I Group ---
 speech_i_short:                 ; /ɪ/ (Sill)
         .byte R11,$0e, R12,$00
         .byte R4,$1e, R5,$00
-        .byte NOISE,$0a, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0a
+        .byte SHORT_SAW
 speech_i_long:                  ; /iː/ (Sil)
         .byte R11,$10, R12,$00
         .byte R4,$1b, R5,$00
-        .byte NOISE,$09, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$09
+        .byte LONG_SAW
 
 ; --- AE (Ä) Group ---
 speech_ae_short:                ; /ɛ/ (Märta)
         .byte R11,$07, R12,$00
         .byte R4,$28, R5,$00
-        .byte NOISE,$0d, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0d
+        .byte SHORT_SAW
 speech_ae_long:                 ; /æː/ (Här)
         .byte R11,$06, R12,$00
         .byte R4,$24, R5,$00
-        .byte NOISE,$0d, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0d
+        .byte LONG_SAW
 
 ; --- AO (Å) Group ---
 speech_ao_short:                ; /ɔ/ (Lång)
         .byte R11,$07, R12,$00
         .byte R4,$48, R5,$00
-        .byte NOISE,$0c, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0c
+        .byte SHORT_SAW
 speech_ao_long:                 ; /oː/ (Mål)
         .byte R11,$06, R12,$00
         .byte R4,$45, R5,$00
-        .byte NOISE,$0c, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0c
+        .byte LONG_SAW
 
 ; --- OE (Ö) Group ---
 speech_oe_short:                ; /œ/ (Nött)
         .byte R11,$09, R12,$00
         .byte R4,$30, R5,$00
-        .byte NOISE,$0e, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0e
+        .byte SHORT_SAW
 speech_oe_long:                 ; /øː/ (Nö)
         .byte R11,$0a, R12,$00
         .byte R4,$2d, R5,$00
-        .byte NOISE,$0e, WAIT4, ENV,LOOP_SAW, RET
+        .byte NOISE,$0e
+        .byte LONG_SAW
